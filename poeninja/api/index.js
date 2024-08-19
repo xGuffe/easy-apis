8291831ca75ee8121958f0bbc3a963086d34ec9f
Guffe <yungguffe@gmail.com>
2024-08-19 19:47:30 +0200

    Using return for res.*

--- poeninja/api/index.js
+++ poeninja/api/index.js
@@ -7,12 +7,12 @@ app.get('/poeninja/:name', (req, res) => {
     const { name } = req.params
     fetch_poe_users().then(data => {
         if (!(name in data)) {
-            res.json({'error': `Could not find the user ${name}`})
+            return res.json({'error': `Could not find the user ${name}`})
         }
-        res.json({'search': name, 'account': data[name], 'url': `https://poe.ninja/builds/streamers/character/${name}/${data[name]}?type=streamers&i=0`})
+        return res.json({'search': name, 'account': data[name], 'url': `https://poe.ninja/builds/streamers/character/${name}/${data[name]}?type=streamers&i=0`})
     }).catch(err => {
         console.error(err)
-        res.status(500).json({'error': 'Failed to fetch POE user'})
+        return res.status(500).json({'error': 'Failed to fetch POE user'})
     })
 })
 
