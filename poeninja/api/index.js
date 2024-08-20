102a91062e106835e9a3d8bebdb401d7f81cde46
Guffe <yungguffe@gmail.com>
2024-08-20 04:16:08 +0200

    Added path to fetch all names

--- poeninja/api/index.js
+++ poeninja/api/index.js
@@ -3,7 +3,13 @@ const app = express()
 const { fetch_poe_users } = require('../src/getpoename')
 
 
-app.get('/poeninja/:name', (req, res) => {
+app.get('/poeninja/streamers', (req, res) => {
+    return fetch_poe_users().then(data => {
+        return res.json(data)
+    })
+})
+
+app.get('/poeninja/streamers/:name', (req, res) => {
     const { name } = req.params
     fetch_poe_users().then(data => {
         if (!(name in data)) {
